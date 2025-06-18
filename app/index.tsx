c47a2c1f73515d4c3c0be2d5d5f751aee74c5920
Jonathan Walker <w@lker.dev>
2025-06-18 12:34:19 +0900

    legend state with no local persistence or remote sync

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,35 @@
 import "@/global.css"
 
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { LegendList } from "@legendapp/list";
+import { observable } from "@legendapp/state";
+import { use$ } from "@legendapp/state/react";
 
 
-const items = [
-	{ id: "1", title: "Test" },
-	{ id: "2", title: "Hello Expo / React Native" },
-	{ id: "3", title: "Go Train" },
-];
+
+
+const state$ = observable({
+	items: [
+		{ id: "1", title: "Test" },
+		{ id: "2", title: "Hello Expo / React Native" },
+		{ id: "3", title: "Go Train" },
+	]
+});
 
 export default function Index() {
+
+	const addAnother = () => state$.items.set((prev) => [...prev, { id: String(prev.length + 1), title: "Another" }])
+
+	const items = use$(state$.items)
+
 	return (
-		<View className="flex-1">
+
+		<View className="flex-1" >
 			<View className="flex-1 justify-center align-center">
-				<Text className="text-center">Top half</Text>
+				<Button
+					title="Add Another"
+					onPress={addAnother}
+				/>
 			</View>
 			<View className="flex-1">
 
@@ -26,6 +41,6 @@ export default function Index() {
 					recycleItems={true}
 				/>
 			</View>
-		</View>
+		</View >
 	);
 }
