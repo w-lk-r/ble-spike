946d36ebd424e32d3340bb52a3e0a91ba62b981b
Jonathan Walker <w@lker.dev>
2025-06-18 12:01:53 +0900

    legend-list

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,31 @@
 import "@/global.css"
 
 import { Text, View } from "react-native";
+import { LegendList } from "@legendapp/list";
+
+
+const items = [
+	{ id: "1", title: "Test" },
+	{ id: "2", title: "Hello Expo / React Native" },
+	{ id: "3", title: "Go Train" },
+];
 
 export default function Index() {
 	return (
-		<View className="flex-1 justify-center items-center">
-			<Text className="text-center text-2xl text-bold">Edit app/index.tsx to edit this screen.</Text>
+		<View className="flex-1">
+			<View className="flex-1 justify-center align-center">
+				<Text className="text-center">Top half</Text>
+			</View>
+			<View className="flex-1">
+
+				<LegendList
+					className="flex-1 justify-center align-center"
+					data={items}
+					renderItem={({ item }) => <Text className="text-center">{item.title}</Text>}
+					keyExtractor={(item) => item.id}
+					recycleItems={true}
+				/>
+			</View>
 		</View>
 	);
 }
