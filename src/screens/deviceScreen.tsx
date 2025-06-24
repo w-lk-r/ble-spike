3a2b6bb34a7bcf9202832712b87ab80f0b858c57
Jonathan Walker <w@lker.dev>
2025-06-24 13:19:08 +0900

    routes

---
+++ src/screens/deviceScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function DeviceScreen() {
+	return (
+		<View className="flex-1 justify-center align-center">
+			<Text className="text-center">Device Screen</Text>
+		</View>
+	)
+}
+
