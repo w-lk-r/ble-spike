1c5ccc8485cee17d2005a99026eb38e7b4290569
Jonathan Walker <w@lker.dev>
2025-06-26 14:00:51 +0900

    discover devices

--- src/screens/deviceScreen.tsx
+++ src/screens/deviceScreen.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Button, Text } from "react-native";
 import { useBle } from "@/src/hooks/useBle";
 import { observer } from '@legendapp/state/react'; // Import observer for React components
-import { bleState$, isBlePoweredOn$ } from "@/src/state/bleStore";
+import { bleState$, isBlePoweredOn$, startScan, foundDevices$ } from "@/src/state/bleStore";
+import { LegendList } from "@legendapp/list";
+
 
 const DeviceScreen = observer(function DeviceScreen() {
 	useBle();
@@ -9,11 +11,26 @@ const DeviceScreen = observer(function DeviceScreen() {
 	const currentBleState = bleState$.get();
 	const isBlePoweredOn = isBlePoweredOn$.get();
 
+	const devices = foundDevices$.get();
+
+
+	const handlePress = () => {
+		startScan()
+		console.log(devices)
+	}
+
 
 	return (
 		<View className="flex-1 justify-center align-center">
-
 			<Text className="text-center">Ble state: {currentBleState} </Text>
+			<Button title="scan" onPress={handlePress} />
+
+			<LegendList
+				className="flex-1 justify-center align-center"
+				data={devices}
+				renderItem={({ item }) => <Text className="text-center">{item.id}</Text>}
+				keyExtractor={(item) => String(item.id)}
+			/>
 		</View>
 	)
 })
