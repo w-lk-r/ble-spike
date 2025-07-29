c4efddd4e85511e301078dad308df0a1ea09740c
Jonathan Walker <w@lker.dev>
2025-07-29 11:06:43 +0900

    ui update

--- src/screens/deviceScreen.tsx
+++ src/screens/deviceScreen.tsx
@@ -1,8 +1,9 @@
 import { View, Button, Text } from "react-native";
 import { useBle } from "@/src/ble/useBle";
 import { observer } from '@legendapp/state/react'; // Import observer for React components
-import { bleManagerState$, scan, foundDevices$ } from "@/src/ble/bleStore";
+import { bleManagerState$, scan, foundDevices$, connectToDevice, connectionStates$ } from "@/src/ble/bleStore";
 import { LegendList } from "@legendapp/list";
+import { ConnectionStates } from "@/src/ble/types";
 
 
 const DeviceScreen = observer(function DeviceScreen() {
@@ -10,6 +11,13 @@ const DeviceScreen = observer(function DeviceScreen() {
 
 	const currentBleState = bleManagerState$.get();
 	const devices = foundDevices$.get();
+	const connections = connectionStates$.get();
+	
+	// Filter devices to show only connected ones
+	const connectedDevices = devices.filter(device => 
+		device.connectionState === ConnectionStates.Connected
+	);
+
 
 	return (
 		<View className="flex-1 justify-center align-center">
@@ -19,9 +27,21 @@ const DeviceScreen = observer(function DeviceScreen() {
 			<LegendList
 				className="flex-1 justify-center align-center"
 				data={devices}
-				renderItem={({ item }) => <Text className="text-center">{item.id}</Text>}
+				renderItem={({ item }) => <Text onPress={() => connectToDevice(item.id)} className="text-center">{item.id}</Text>}
 				keyExtractor={(item) => String(item.id)}
+
+
 			/>
+			<Text className="text-center">Connected Devices: {connectedDevices.length} </Text>
+			<LegendList
+				className="flex-1 justify-center align-center"
+				data={connectedDevices}
+				renderItem={({ item }) => <Text className="text-center">{item.name || item.id}</Text>}
+				keyExtractor={(item) => String(item.id)}
+
+
+			/>
+
 		</View>
 	)
 })
